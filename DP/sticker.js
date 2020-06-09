6c52c3c5f0c9cb5d25c144f379753076897bfb92
color0e <limjangsoon@naver.com>
2020-06-09 13:33:09 +0900

    sticker add DP

--- DP/sticker.js
+++ DP/sticker.js
@@ -2,28 +2,47 @@ let stickers = [[50,10,100,20,40],[30,50,70,10,60]];
 let stickers2 = [[10,30,10,50,100,20,40],[20,40,30,50,60,20,80]];
 
 let Desk = (s) => {
+    let DP = new Map();
     let getMaxpoint = (index,prev)=>{
+        let result;
+        if(DP.get(index+"_"+prev)!==undefined){
+            return DP.get(index+"_"+prev)
+        }
+
         if(index==s[0].length-1){
             switch(prev){
                 case 0:
-                    return s[0][index]>s[1][index]?s[0][index]:s[1][index];
+                    result=s[0][index]>s[1][index]?s[0][index]:s[1][index];
+                    break;
                 case 1:
-                    return s[1][index];
+                    result=s[1][index];
+                    break;
                 case 2:
-                    return s[0][index];
+                    result=s[0][index];
+                    break;
             } 
+            DP.set(index+"_"+prev,result);
+            return result;
         }
+
         switch(prev){
             case 0:
-                return Math.max.apply(null,[getMaxpoint(index+1,0),s[0][index]+getMaxpoint(index+1,1),s[1][index]+getMaxpoint(index+1,2)]);
+                result = Math.max.apply(null,[getMaxpoint(index+1,0),s[0][index]+getMaxpoint(index+1,1),s[1][index]+getMaxpoint(index+1,2)]);
+                break;
             case 1:
-                return Math.max.apply(null,[getMaxpoint(index+1,0),s[1][index]+getMaxpoint(index+1,2)]);
+                result = Math.max.apply(null,[getMaxpoint(index+1,0),s[1][index]+getMaxpoint(index+1,2)]);
+                break;
             case 2:
-                return Math.max.apply(null,[getMaxpoint(index+1,0),s[0][index]+getMaxpoint(index+1,1)]);
+                result = Math.max.apply(null,[getMaxpoint(index+1,0),s[0][index]+getMaxpoint(index+1,1)]);
+                break;
         }
+        
+        DP.set(index+"_"+prev,result);
+        return result;
     }
 
     console.log(getMaxpoint(0,0));
+    console.log(DP);
 }
 
 Desk(stickers);
