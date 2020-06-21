28977377423627e2bbec67f521deca1b9afe73a9
color0e <limjangsoon@naver.com>
2020-06-21 16:56:15 +0900

    squared sum

---
+++ DP/Squaredsum.js
@@ -0,0 +1,28 @@
+let squaredSum = (number) => {
+
+    let squared = (num) => {
+        return num * num;
+    }
+
+    let sum = 0;
+    let count = 0;
+    let temp = squared(number-1)
+    let k = 1;
+    while(number != sum){
+
+        if(number < sum){
+            sum = sum - temp;
+            k = k + 1;
+            count--;
+            continue;
+        }
+
+        temp = squared(number-k);
+        sum = sum + temp;
+        count++;
+    }
+    
+    return count;
+}
+
+console.log(squaredSum(7));
